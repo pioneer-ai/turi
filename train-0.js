b09113d038da71354c93a7f7be5b62375d0f148d
Hayden <95329795+proplayer919@users.noreply.github.com>
2023-08-02 08:47:04 +1000

    npg-0.1-rc1

---
+++ train-0.js
@@ -0,0 +1,27 @@
+const { NlpManager } = require('node-nlp');
+const fs = require('fs');
+
+let trainingData = getTrainingData();
+
+// Train the model
+async function trainModel() {
+    // Create an instance of the NLP manager
+    const manager = new NlpManager({ languages: ['en'] });
+
+    trainingData.forEach((element, index) => {
+        manager.addDocument('en', element.input, 'message.' + index.toString());
+        manager.addAnswer('en', 'message.' + index.toString(), element.output);
+    });
+
+    await manager.train();
+    await manager.save('./npg-0.nlp');
+    
+    console.log("NPG-0 Model Params: " + trainingData.length.toString());
+    console.log("NPG-0 Model trained and saved successfully!");
+}
+
+function getTrainingData() {
+    return JSON.parse(fs.readFileSync("./scribe-datset-0.json"));
+}
+
+trainModel();
