7a5efc21ce0aefb2473337cdddcaf56dcd7c2e6e
Hayden <95329795+proplayer919@users.noreply.github.com>
2023-08-02 10:04:18 +1000

    add npg-0.1-rc1 to publc avalibility

--- index.js
+++ index.js
@@ -8,24 +8,28 @@ const app = express();
 const port = 8080;
 
 // Create an instance of the NLP manager
-const manager = new NlpManager({ languages: ['en'] });
+const manager0 = new NlpManager({ languages: ['en'] });
+
+// Create an instance of the NLP manager
+const manager1 = new NlpManager({ languages: ['en'] });
 
 // Load the trained model
 async function loadModel() {
-    await manager.load('./npg-0.nlp');
+    await manager0.load('./npg-0.nlp');
+    await manager1.load('./npg-0.1-rc1.nlp');
 }
 
 // Train and save the model
 loadModel()
     .then(() => {
-        console.log('NPG-0 model loaded successfully.');
+        console.log('NPG-0 & NPG-0.1-rc1 model loaded successfully.');
         // Start the server after training the model
         app.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
         });
     })
     .catch((err) => {
-        console.error('Error loading the model:', err);
+        console.error('Error loading the models:', err);
     });
 
 // Set up the Express routes
@@ -49,9 +53,9 @@ app.get("/info", (req, res) => {
     });
 });
 
-// Serve ./chat/index.html on /chat
-app.get('/chat', (req, res) => {
-    fs.readFile('./chat/index.html', 'utf8', (error, data) => {
+// Serve ./chat/index-0.html on /chat
+app.get('/chat/0', (req, res) => {
+    fs.readFile('./chat/index-0.html', 'utf8', (error, data) => {
         if (error) {
             console.error('Error reading file:', error);
             res.status(500).send('Internal Server Error');
@@ -61,8 +65,20 @@ app.get('/chat', (req, res) => {
     });
 });
 
-// Handle POST requests to /api/chat endpoint
-app.post('/api/chat', async (req, res) => {
+// Serve ./chat/index-0.1-rc1.html on /chat
+app.get('/chat/0.1-rc1', (req, res) => {
+    fs.readFile('./chat/index-0.1-rc1.html', 'utf8', (error, data) => {
+        if (error) {
+            console.error('Error reading file:', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+// Handle POST requests to /api/chat/npg-0 endpoint
+app.post('/api/chat/npg-0', async (req, res) => {
     let message = req.body.prompt.toLowerCase();
     let response;
 
@@ -76,7 +92,31 @@ app.post('/api/chat', async (req, res) => {
     }
 
     try {
-        response = await manager.process('en', message);
+        response = await manager0.process('en', message);
+        const answer = response.answer || 'Sorry, I do not understand.';
+        res.send({ answer });
+    } catch (error) {
+        console.error('Error processing message:', error);
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
+});
+
+// Handle POST requests to /api/chat/npg-0.1-rc1 endpoint
+app.post('/api/chat/npg-0.1-rc1', async (req, res) => {
+    let message = req.body.prompt.toLowerCase();
+    let response;
+
+    // Check for explicit content asynchronously
+    const hasExplicitContent = await containsExplicitContent(message);
+
+    if (hasExplicitContent) {
+        response = "I'm sorry, but I cannot respond to that request.";
+        res.send({ answer: response });
+        return;
+    }
+
+    try {
+        response = await manager1.process('en', message);
         const answer = response.answer || 'Sorry, I do not understand.';
         res.send({ answer });
     } catch (error) {
